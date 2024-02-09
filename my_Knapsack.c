0b6ba08be712376922250b2e34f60f7815c5735b
noamc <noam12345002@gmail.com>
2024-02-09 13:12:57 +0200

    update 9/2/24

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -3,12 +3,11 @@
 #define CAPACITY 20
 #define MAX_LEN 21
 
-
 int knapSack(int weights[SIZE], int values[SIZE], int selected_bool[SIZE]);
 int max(int x, int y);
 int main()
 {
-    printf("start");
+    // printf("start");
     char product[SIZE][MAX_LEN];
     int values[SIZE];
     int weights[SIZE];
@@ -20,45 +19,90 @@ int main()
         scanf("%d", &weights[i]);
     }
     int maxVul = knapSack(weights, values, result);
-   // printf("%d", maxVul);
+    // printf("%d", maxVul);
+    // Print:
+    printf("Maximum profit: %d\n", maxVul);
+
+    printf("Selected items:");
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (result[i] == 1)
+        {
+            printf(" %s", product[i]);
+        }
+    }
+
     return 0;
 }
 
-int knapSack(int weights[SIZE], int values[SIZE], int selected_bool[SIZE]) {
-    int matrix[SIZE+1][CAPACITY+1];//0-15
+int knapSack(int weights[SIZE], int values[SIZE], int selected_bool[SIZE])
+{
+    int matrix[SIZE + 1][CAPACITY + 1]; // 0-15
 
-    for (int i = 0; i < CAPACITY+1; i++) {
+    for (int i = 0; i < CAPACITY + 1; i++)
+    {
         matrix[0][i] = 0;
     }
-    for (int i = 0; i < SIZE+1; i++) {
+    for (int i = 0; i < SIZE + 1; i++)
+    {
         matrix[i][0] = 0;
     }
 
-    for (int i = 1; i < SIZE+1; i++) {  // Starting from 1
-        for (int B = 1; B < CAPACITY+1; B++) {  // Starting from 1
-            if (weights[i-1] > B) {
+    for (int i = 1; i < SIZE + 1; i++)
+    { // Starting from 1
+        for (int B = 1; B < CAPACITY + 1; B++)
+        { // Starting from 1
+            if (weights[i - 1] > B)
+            {
                 matrix[i][B] = matrix[i - 1][B];
-            } else {
-                matrix[i][B] = max(matrix[i - 1][B], values[i-1] + matrix[i - 1][B - weights[i-1]]);
+            }
+            else
+            {
+                matrix[i][B] = max(matrix[i - 1][B], values[i - 1] + matrix[i - 1][B - weights[i - 1]]);
             }
         }
+    }
+    // Print the matrix
+    // printf("Matrix:\n");
+    // for (int i = 0; i < SIZE + 1; i++)
+    // {
+    //     for (int j = 0; j < CAPACITY + 1; j++)
+    //     {
+    //         printf("%d\t", matrix[i][j]);
+    //     }
+    //     printf("\n");
+    // }
 
-    }
-// Print the matrix
-    printf("Matrix:\n");
-    for (int i = 0; i < SIZE+1; i++) {
-        for (int j = 0; j < CAPACITY+1; j++) {
-            printf("%d\t", matrix[i][j]);
+    int i = SIZE;
+    int j = CAPACITY;
+    while (i>0 && j>0)
+    {
+        if (matrix[i][j] > matrix[i-1][j])
+        {
+            selected_bool[i-1] = 1;
+
+            j = j -weights[i-1];
         }
-        printf("\n");
+        else
+        {
+            selected_bool[i-1] = 0;
+        }
+        i=i-1;
     }
-    // Return the maximum value in the last cell of the matrix
+
+
+    // Return the maximum value
     return matrix[SIZE][CAPACITY];
 }
-int max(int x, int y){
-    if(x > y){
+
+int max(int x, int y)//return the max value between x and y
+{
+    if (x > y)
+    {
         return x;
-    }else{
+    }
+    else
+    {
         return y;
     }
 }
